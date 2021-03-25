5200f9a3821abd062bade6162d5611b487cff3f0
adonay <adonaye84@gmail.com>
2021-03-24 21:55:09 -0600

    AdonayGarcia
    Segundo Commit

--- Ejercicio 1/IdiomasPorArray.php
+++ Ejercicio 1/IdiomasPorArray.php
@@ -54,7 +54,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="css/styleArrays.css" rel="stylesheet" type="text/css" />
     <title>Academia de Idiomas</title>
 </head>
 <body>
